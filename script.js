9342123eed8e53045fb17d491ab26388a5226f73
Deepak Singh <deepaksngh100@gmail.com>
2022-02-09 14:40:20 +0530

    challenge 1 and 2 array

--- script.js
+++ script.js
@@ -1,13 +1,70 @@
-// function declartion 
-function callName(name) {
-    console.log(`my name is ${name}`);
-}
+// // function declartion 
+// function callName(name) {
+//     console.log(`my name is ${name}`);
+// }
 
-// function xepression 
-let callRollNo = function (rollNo) {
-    console.log(`your roll number is ${rollNo}`);
-}
-// Arrow fucntion 
-const calAge = birthYear => 2037 - birthYear;
+// // function xepression 
+// let callRollNo = function (rollNo) {
+//     console.log(`your roll number is ${rollNo}`);
+// }
+// // Arrow fucntion 
+// const calAge = birthYear => 2037 - birthYear;
+// console.log(calAge(2020));
 
-console.log(calAge(2020));
+// // chhallenge 
+// function calcAverage(socre1, score2, score3) {
+//     let avgScore = (socre1 + score2 + score3) / 3;
+//     return avgScore;
+// }
+
+// function checkWinner(avgDolphins, avgkoolas) {
+//     if (avgDolphins > 2 * avgkoolas) {
+//         console.log(`Dolphins win , final score is ${avgDolphins} and for koalas is ${avgkoolas}`);
+//     } else if (avgkoolas > 2 * avgDolphins) {
+//         console.log(`Koalas win , final score is ${avgkoolas} and for koalas is ${avgDolphins}`);
+//     } else {
+//         console.log("none scored twice the other team");
+//     }
+// }
+
+// // let dolphins = calcAverage(44, 23, 71);
+// // let koalas = calcAverage(65, 54, 49);
+// let dolphins = calcAverage(85, 54, 41);
+// let koalas = calcAverage(23, 34, 27);
+// checkWinner(dolphins, koalas);
+
+// array methods 
+
+// challeneg 2 array
+let bills = [125, 555, 44];
+let tips = [];
+let totals = [];
+for (let n = 0; n < bills.length; n++) {
+    let tip;
+    let total;
+    function calTip(bill) {
+        if (bills[n] <= 300 && bills[n] >= 50) {
+            tip = bills[n] * 0.15;
+            total = bills[n] + tip;
+            tips.push(tip);
+            totals.push(total);
+        }
+        else {
+            tip = bills[n] * 0.2;
+            total = bills[n] + tip;
+            tips.push(tip);
+            totals.push(total);
+        }
+    }
+    calTip(bills[n]);
+
+}
+console.log(tips);
+console.log(totals);
+
+const profile = {
+    firstName: "Deepak",
+    lastName: "Singh",
+    pincode: "143001"
+}
+console.log(profile);
