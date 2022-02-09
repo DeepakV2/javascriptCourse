5bb56ba3046ae77977bca905c15b87e30052a244
Deepak Singh <deepaksngh100@gmail.com>
2022-02-09 12:40:11 +0530

    func declaration and expression

---
+++ script.js
@@ -0,0 +1,10 @@
+// function declartion 
+function callName(name) {
+    console.log(`my name is ${name}`);
+}
+
+// function xepression 
+let callRollNo = function (rollNo) {
+    console.log(`your roll number is ${rollNo}`);
+}
+// Arrow fucntion 
